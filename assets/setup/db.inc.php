e8de8d4826966c98be145c6a6a012423e40cc040
vmatteo81 <103205571+vmatteo81@users.noreply.github.com>
2022-04-17 06:49:46 +0000

    update

--- assets/setup/db.inc.php
+++ assets/setup/db.inc.php
@@ -9,3 +9,29 @@ if (!$conn)
 {
     die("Connection failed: ". mysqli_connect_error());
 }
+else
+{
+    if (!defined('MAIL_HOST'))
+    {
+        $sql = "select host,username,password,encryption,port from mail_config;";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+    
+            return $_SESSION['ERRORS']['scripterror'] = 'SQL error';
+        } 
+        else {
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);   
+            if (!$row = mysqli_fetch_assoc($result)) {
+                echo ("mail_config error on db");
+            }
+            else {
+                define('MAIL_HOST', $row['host']);
+                define('MAIL_USERNAME', $row['username']);
+                define('MAIL_PASSWORD', $row['password']);
+                define('MAIL_ENCRYPTION', $row['encryption']);
+                define('MAIL_PORT', $row['port']);        
+            }
+        }
+    }     
+}
--- assets/setup/env.php
+++ assets/setup/env.php
@@ -12,9 +12,3 @@ if (!defined('DB_HOST'))                        define('DB_HOST','localhost');
 if (!defined('DB_USERNAME'))                    define('DB_USERNAME','root');
 if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'');
 if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
-
-if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp-relay.sendinblue.com');
-if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'vmatteo23@yahoo.it');
-if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'Z94Bmw0EqDJHGS1n');
-if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'tls');
-if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 587);
