9e9aa88b4e5445418090715bcc01456beff7035e
vmatteo81 <103205571+vmatteo81@users.noreply.github.com>
2022-05-09 13:17:32 +0000

    Update env.php

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -1,14 +1,14 @@
 <?php
 
-if (!defined('APP_NAME'))                       define('APP_NAME', 'KycDefiWallet');
-if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'DefiAuthority');
+if (!defined('APP_NAME'))                       define('APP_NAME', 'ReCrypto');
+if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'ReCrypto');
 if (!defined('APP_OWNER'))                      define('APP_OWNER', 'vmatteo81');
-if (!defined('APP_DESCRIPTION'))                define('APP_DESCRIPTION', 'add Kyc to defi wallet');
-if (!defined('APP_WEBSITE'))                    define('APP_WEBSITE', '20.113.26.252/kycdefiwallet');
+if (!defined('APP_DESCRIPTION'))                define('APP_DESCRIPTION', 'Real Estate Crypto');
+if (!defined('APP_WEBSITE'))                    define('APP_WEBSITE', '20.113.26.252/ReCrypto');
 
 if (!defined('ALLOWED_INACTIVITY_TIME'))        define('ALLOWED_INACTIVITY_TIME', time()+1*60);
 
-if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'kycdefiwallet');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'recrypto');
 if (!defined('DB_HOST'))                        define('DB_HOST','localhost');
 if (!defined('DB_USERNAME'))                    define('DB_USERNAME','root');
 if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'');
