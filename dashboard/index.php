f98422a7af00e1e0cbbaf1da1cf64153d896f8f5
vmatteo23 <99023386+vmatteo23@users.noreply.github.com>
2023-08-04 15:31:00 +0200

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -18,7 +18,7 @@ check_verified();
         <div class="col-sm-9">
 
             <div class="d-flex align-items-center p-3 mt-5 mb-3 text-white-50 bg-purple rounded box-shadow">
-                <img class="mr-3" src="../assets/images/logonotextwhite.png" alt="" width="48" height="48">
+                <img class="mr-3" src="../assets/images/logo.png" alt="" width="48" height="48">
                 <div class="lh-100">
                     <h6 class="mb-0 text-white lh-100">Admin Dashboard</h6>
                     <small>[Development in Progress]</small>
